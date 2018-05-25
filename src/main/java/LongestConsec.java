f03c56517798f120c850ee3a01497e90189a1019
Michael Kalisvaart <mkalisvaart@bol.com>
2018-05-25 16:02:03 +0200

    2 Kata: Money + LongestConsec

---
+++ src/main/java/LongestConsec.java
@@ -0,0 +1,30 @@
+class LongestConsec {
+    
+    public static String longestConsec(String[] strarr, int k) {
+        int n = strarr.length;
+        if (n == 0 || k > n || k <= 0)
+            return "";
+        
+        String maxConsec = "";
+        boolean stop = false;
+        int max = 0;
+        for (int i = 0; i < strarr.length; i++) {
+            StringBuilder consec = new StringBuilder();
+            for (int j = 0; j < k; j++) {
+                if (i + j >= strarr.length) {
+                    stop = true;
+                    break;
+                }
+                consec.append(strarr[i + j]);
+            }
+            if (stop) {
+                break;
+            } else if (consec.length() > max) {
+                max = consec.length();
+                maxConsec = consec.toString();
+            }
+            
+        }
+        return maxConsec;
+    }
+}
---
+++ src/main/java/Money.java
@@ -0,0 +1,12 @@
+public class Money {
+    public static int calculateYears(double principal, double interest,  double tax, double desired) {
+        int years = 0;
+        while (principal < desired) {
+            double i = principal * interest;
+            i -= i * tax;
+            principal += i;
+            years++;
+        }
+        return years;
+    }
+}
