d582cf1a47526f99b86ef210921755778dbd5918
Michael Kalisvaart <mkalisvaart@bol.com>
2018-05-28 10:56:32 +0200

    XO kata

---
+++ src/main/java/XO.java
@@ -0,0 +1,19 @@
+public class XO {
+    public static boolean getXO(String str) {
+        String[] letters = str.toLowerCase().split("");
+        int x = 0, o = 0;
+        
+        for (String letter : letters) {
+            if (letter.equals("x"))
+                x++;
+            else if (letter.equals("o"))
+                o++;
+        }
+        
+        return x == o;
+        
+        //clever
+        //str = str.toLowerCase();
+        //return str.replace("o","").length() == str.replace("x","").length();
+    }
+}
